311e6f374f62b00dcc85b32e9f6386d881db88fa
Mavel Portia Antwi <meyvel22@gmail.com>
2022-04-07 08:12:42 -0700

    Improved 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - entry point
- * 
- * Return: 0 after printing the function
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\Programming is like building a multilingual puzzle");
 	return (0);
 }
