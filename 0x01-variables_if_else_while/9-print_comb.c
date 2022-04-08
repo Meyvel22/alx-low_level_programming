f66950c232cf4d80ce168506bef153fde8449bc6
Mavel Portia Antwi <meyvel22@gmail.com>
2022-04-08 09:11:49 -0700

    Added task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers between 0 to 9 with comms and spaces
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+	
+	while (c < 10)
+	{
+	putchar(48 + c);
+	if (c != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
