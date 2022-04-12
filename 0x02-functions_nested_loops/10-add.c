94d0cfaf157dbb92ae749fe4f726b0e40e2c1a7f
Mavel Portia Antwi <meyvel22@gmail.com>
2022-04-12 08:45:24 -0700

    Added task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int add - adding two integers
+ *
+ * Return: Always 0.
+ */
+int add(int, int)
+{
+	int a;
+	int b;
+
+	int add(int a, int b)
+	{
+		return (a + b);
+	}
+}
