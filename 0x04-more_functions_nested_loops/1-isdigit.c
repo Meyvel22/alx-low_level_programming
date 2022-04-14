1d59f50926b6be4c5500bf45dacbf16191854fc5
Mavel Portia Antwi <meyvel22@gmail.com>
2022-04-14 07:48:29 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - Returns 1 if it is a digit between 0 and 9 and 0 otherwise
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	for (c = 0; c < 10; c++)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14.
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int count;
+
+	count = 0;
+	while (count < 0)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i);
+		}
+	count++;
+	_putchar('\n');
+	}
+}
