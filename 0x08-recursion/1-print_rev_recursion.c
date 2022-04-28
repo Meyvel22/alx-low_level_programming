ae116accc0bda913864018968703995a4f2236e6
Mavel Portia Antwi <meyvel22@gmail.com>
2022-04-28 09:21:15 -0700

    Done

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,14 +3,14 @@
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: string
- * Return: void
+ * Return: reversed string
  */
 
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
 		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
