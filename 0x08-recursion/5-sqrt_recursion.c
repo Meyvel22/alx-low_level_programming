c418e4fcae82a42337abf4f4206062e937bb3500
Mavel Portia Antwi <meyvel22@gmail.com>
2022-04-28 09:28:12 -0700

    Finished

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,7 +9,7 @@
 int isqrt(int x, int y)
 {
 	if ((y * y) == x)
-		return (x);
+		return (y);
 	else if ((y * y) > x)
 		return (-1);
 	return (isqrt(x, y + 1));
@@ -25,5 +25,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (isqrt(n, 1));
+	return (isqrt(n, 0));
 }
