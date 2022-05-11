4fe6968beb62ec876834b485a59979ddfba55368
Mavel Portia Antwi <meyvel22@gmail.com>
2022-05-11 08:01:06 -0700

    Structures typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ * @d: pointer to struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of owner of dog
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != 0)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * *new_dog - function that creates a new dog
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: name of owner of new dog
+ * Return: NULL if function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *n_dog;
+	int x, pname, powner;
+
+	n_dog = malloc(sizeof(*n_dog));
+	if (n_dog == NULL || !(name) || !(owner))
+	{
+		free(n_dog);
+		return (NULL);
+	}
+
+	for (pname = 0; name[pname]; pname++)
+		;
+	for (powner = 0; owner[powner]; powner++)
+		;
+
+	(*n_dog).name = malloc(pname + 1);
+	(*n_dog).owner = malloc(powner + 1);
+
+	if (!((*n_dog).name) || !((*n_dog).owner))
+	{
+		free((*n_dog).name);
+		free((*n_dog).owner);
+		free(*n_dog);
+		return (NULL);
+	}
+
+	for (x = 0; x < pname; x++)
+		(*n_dog).name[x] = name[x];
+	(*n_dog).name[x] = '\0';
+
+	(*n_dog).age = age;
+
+	for (x = 0; x < powner; x++)
+		(*n_dog).owner[x] = owner[x];
+	(*n_dog).owner[x] = '\0';
+
+	return (n_dog);
+}
