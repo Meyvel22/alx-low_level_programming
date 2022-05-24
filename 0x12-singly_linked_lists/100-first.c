52fd3b072695e3be7b273ebfdaa8ef4ff4d826da
Mavel Portia Antwi <meyvel22@gmail.com>
2022-05-24 10:09:08 -0700

    Changes

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * print_tense - prints a string before main
+ * print - prints a string before main
  *
  * Return: void
  */
 
-void print_tense(void)
+void print(void)
 {
 	printf("You're beat! and yet you must allow,");
 	printf("\nI bore my house upon my back!\n");
