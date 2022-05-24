4fa79a7a999c77d7118a97fad3947c0479fbb423
Mavel Portia Antwi <meyvel22@gmail.com>
2022-05-24 11:22:20 -0700

    Done t

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * initial - prints a string before main
+ * initial - function that prints before main
  *
  * Return: void
  */
 
 void __attribute__ ((constructor)) initial()
 {
-	printf("You're beat! and yet you must allow,\n"
+	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
 }
