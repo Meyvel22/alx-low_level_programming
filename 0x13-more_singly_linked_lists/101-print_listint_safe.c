4834f31d294256273cd1472ad879f0fd26c44009
Mavel Portia Antwi <meyvel22@gmail.com>
2022-05-26 13:08:49 -0700

    Complete

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,69 +1,88 @@
 #include "lists.h"
+#include <stdio.h>
+
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 /**
- * free_listp - frees a linked list
- * @head: head of a list.
+ * looped_listint_len - Counts the number of unique nodes
+ *                      in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
  *
- * Return: no return.
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nodes in the list.
  */
-void free_listp(listp_t **head)
+size_t looped_listint_len(const listint_t *head)
 {
-	listp_t *temp;
-	listp_t *curr;
+	const listint_t *coot, *buck;
+	size_t  apex = 1;
 
-	if (head != NULL)
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	coot = head->next;
+	buck = (head->next)->next;
+
+	while (buck)
 	{
-		curr = *head;
-		while ((temp = curr) != NULL)
+		if (coot == buck)
 		{
-			curr = curr->next;
-			free(temp);
+			coot = head;
+			while (coot != buck)
+			{
+				apex++;
+				coot = coot->next;
+				buck = buck->next;
+			}
+
+			coot = coot->next;
+			while (coot != buck)
+			{
+				apex++;
+				coot = coot->next;
+			}
+
+			return (apex);
 		}
-		*head = NULL;
+
+		coot = coot->next;
+		buck = (buck->next)->next;
 	}
+
+	return (0);
 }
 
 /**
- * print_listint_safe - prints a linked list.
- * @head: head of a list.
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
  *
- * Return: number of nodes in the list.
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nnodes = 0;
-	listp_t *hptr, *new, *add;
+	size_t apex, index = 0;
 
-	hptr = NULL;
-	while (head != NULL)
+	apex = looped_listint_len(head);
+
+	if (apex == 0)
 	{
-		new = malloc(sizeof(listp_t));
-
-		if (new == NULL)
-			exit(98);
-
-		new->p = (void *)head;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-
-		while (add->next != NULL)
+		for (; head != NULL; apex++)
 		{
-			add = add->next;
-			if (head == add->p)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_listp(&hptr);
-				return (nnodes);
-			}
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		nnodes++;
 	}
 
-	free_listp(&hptr);
-	return (nnodes);
+	else
+	{
+		for (index = 0; index < apex; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (apex);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,72 +1,96 @@
 #include "lists.h"
 
-/**
- * free_listp2 - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
-void free_listp2(listp_t **head)
-{
-	listp_t *temp;
-	listp_t *curr;
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
 
-	if (head != NULL)
+/**
+ * looped_listint_count - Counts the number of unique nodes
+ *                      in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nodes in the list.
+ */
+size_t looped_listint_count(listint_t *head)
+{
+	listint_t *coot, *buck;
+	size_t apex = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	coot = head->next;
+	buck = (head->next)->next;
+
+	while (buck)
 	{
-		curr = *head;
-		while ((temp = curr) != NULL)
+		if (coot == buck)
 		{
-			curr = curr->next;
-			free(temp);
+			coot = head;
+			while (coot != buck)
+			{
+				apex++;
+				coot = coot->next;
+				buck = buck->next;
+			}
+
+			coot = coot->next;
+			while (coot != buck)
+			{
+				apex++;
+				coot = coot->next;
+			}
+
+			return (apex);
 		}
-		*head = NULL;
+
+		coot = coot->next;
+		buck = (buck->next)->next;
 	}
+
+	return (0);
 }
 
 /**
- * free_listint_safe - frees a linked list.
- * @h: head of a list.
+ * free_listint_safe - Frees a listint_t list safely (ie.
+ *                     can free lists containing loops)
+ * @h: A pointer to the address of
+ *     the head of the listint_t list.
  *
- * Return: size of the list that was freed.
+ * Return: The size of the list that was freed.
+ *
+ * Description: The function sets the head to NULL.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nnodes = 0;
-	listp_t *hptr, *new, *add;
-	listint_t *curr;
+	listint_t *tmp;
+	size_t apex, index;
 
-	hptr = NULL;
-	while (*h != NULL)
+	apex = looped_listint_count(*h);
+
+	if (apex == 0)
 	{
-		new = malloc(sizeof(listp_t));
-
-		if (new == NULL)
-			exit(98);
-
-		new->p = (void *)*h;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-
-		while (add->next != NULL)
+		for (; h != NULL && *h != NULL; apex++)
 		{
-			add = add->next;
-			if (*h == add->p)
-			{
-				*h = NULL;
-				free_listp2(&hptr);
-				return (nnodes);
-			}
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
 		}
-
-		curr = *h;
-		*h = (*h)->next;
-		free(curr);
-		nnodes++;
 	}
 
-	*h = NULL;
-	free_listp2(&hptr);
-	return (nnodes);
+	else
+	{
+		for (index = 0; index < apex; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (apex);
 }
